7e5b9a51e56e6abb3e4bdcda09f8f5800ee7b188
Pradipa Y <pdpyyo@gmail.com>
2026-02-27 09:46:30 +0700

    ganti isian about dan buat controller untuk route about

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,12 +6,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public static function middleware(): array
-    {
-        return [
-            'auth',
-            new Middleware('log', only: ['index']),
-            new Middleware('subscribed', except: ['store']),
-        ];
-    }
+	public function about()
+	{
+		return view('about');
+	}
 }
