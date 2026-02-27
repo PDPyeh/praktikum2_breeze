282895fbbb15daead2cdb61aaaa7903ed87bbde0
Pradipa Y <pdpyyo@gmail.com>
2026-02-27 09:27:43 +0700

    membuat page about dan buat navigasi ke about

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public static function middleware(): array
+    {
+        return [
+            'auth',
+            new Middleware('log', only: ['index']),
+            new Middleware('subscribed', except: ['store']),
+        ];
+    }
 }
