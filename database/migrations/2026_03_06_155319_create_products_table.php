fd2f08954b07ed42c975d5d7b1b1e02ea6532c0a
Pradipa Y <pdpyyo@gmail.com>
2026-03-06 23:08:16 +0700

    mengubah file migrations

--- database/migrations/2026_03_06_155319_create_products_table.php
+++ database/migrations/2026_03_06_155319_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('qty');
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
